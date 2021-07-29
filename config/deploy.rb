636dd320e2e94fe25703fa97861cb1d56c9eb58f
martin kiwala <martinkiwala4@gmail.com>
2021-07-29 22:10:41 +0300

    Syntax error fix in cap file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.16.0"
 
-set :application, "Lukoma Medical Store"
+set :application, "Lukoma-Medical-Store"
 set :repo_url, "git@bitbucket.org:kiwala22/lukoma-medical-store.git"
 
 # Default branch is :master
